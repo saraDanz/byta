08146870720fb5d40ca9574ccd709dada8e03419
saraDanz <104499672+saraDanz@users.noreply.github.com>
2022-12-19 23:57:43 +0200

    Update VARIABLES.js

--- client/src/VARIABLES.js
+++ client/src/VARIABLES.js
@@ -1,4 +1,4 @@
 export const BASE_URL_DEV = "http://localhost:8000/";
-export const BASE_URL="http://localhost:8000/";
+//export const BASE_URL="http://localhost:8000/";
 // 
-// export const BASE_URL = "https://reportbyta.herokuapp.com/";
+export const BASE_URL = "https://reportbyta.herokuapp.com/";
