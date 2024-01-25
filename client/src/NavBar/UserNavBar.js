32bbb040841d30810ee2aa54242285634a28eb80
saraDanz <104499672+saraDanz@users.noreply.github.com>
2024-01-25 12:21:02 +0200

    Update UserNavBar.js

--- client/src/NavBar/UserNavBar.js
+++ client/src/NavBar/UserNavBar.js
@@ -7,7 +7,7 @@ import { Tooltip } from "@mui/material";
 import TeacherNavBar from "./TeacherNavBar";
 const UserNavBar = ({ userRole }) => {
     return (<> 
-        {userRole == 1||userRole==4 && <TeacherNavBar />}
+        {(userRole == 1||userRole==4 )&& <TeacherNavBar />}
         {userRole == 2 && <DirectorNavBar />}
         {userRole == 3 && <ManagerNavBar />}
 
