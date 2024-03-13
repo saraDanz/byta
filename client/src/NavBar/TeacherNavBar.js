aee379c96c856a9b9faf6010282428ba54d75702
saraDanz <104499672+saraDanz@users.noreply.github.com>
2024-03-13 10:05:45 +0200

    Update TeacherNavBar.js

--- client/src/NavBar/TeacherNavBar.js
+++ client/src/NavBar/TeacherNavBar.js
@@ -6,10 +6,10 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 const TeacherNavBar = () => {
     return (
         <>  <Menu.Item >  <Link to="displayCalendar">לוח שנה</Link></Menu.Item>
-             {/*<Menu.Item >
+             <Menu.Item >
                <Link to="tableTeacher" title="הורדת דיווחים">
                     <FileDownloadOutlinedIcon />
-    </Link></Menu.Item>*/}</>
+    </Link></Menu.Item></>
     )
 }
 export default TeacherNavBar;
